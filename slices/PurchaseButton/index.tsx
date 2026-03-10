9ab578784ef77a924d0948099c4830a66a67a3f0
DavideCapuozzo <devsoloweb@gmail.com>
2026-03-10 21:58:12 +0100

    feat: enhance PurchaseButton with loading state and button click handling

--- slices/PurchaseButton/index.tsx
+++ slices/PurchaseButton/index.tsx
@@ -1,22 +1,31 @@
-import { FC } from "react";
+"use client"
+
+
+import { FC, useState } from "react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import { Bounded } from "@/components/Bounded";
 import { PrismicText, PrismicRichText } from "@prismicio/react";
 import { FadeIn } from "@/components/FadeIn";
 import clsx from "clsx";
-import { LuChevronRight } from "react-icons/lu";
+import { LuChevronRight, LuLoader } from "react-icons/lu";
 
-/**
- * Props for `PurchaseButton`.
- */
+
 export type PurchaseButtonProps =
   SliceComponentProps<Content.PurchaseButtonSlice>;
 
-/**
- * Component for "PurchaseButton" Slices.
- */
+
 const PurchaseButton: FC<PurchaseButtonProps> = ({ slice }) => {
+
+  const [isPressed, setIsPressed] = useState(false);
+
+  const handlePurchaseClick = async () => {
+    setIsPressed(true)
+    // TODO: add checkout logic later
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    setIsPressed(false)
+  }
+
   return (
     <Bounded
       data-slice-type={slice.slice_type}
@@ -31,11 +40,23 @@ const PurchaseButton: FC<PurchaseButtonProps> = ({ slice }) => {
           <PrismicText field={slice.primary.heanding} />
         </h2>
 
-        <button className={clsx("group relative w-full overflow-hidden rounded-full border-8 border-gray-900 bg-linear-to-r/oklch from-sky-300 to-sky-600 px-6 py-6 ease-out focus:ring-24 focus:ring-sky-500/50 focus:outline-none motion-safe:transition-all motion-safe:duration-300 md:border-12 md:px-20 md:py-16", "hover:scale-105 hover:shadow-2xl hover:shadow-sky-500/40", "active:scale-95", "cursor-pointer")}>
+        <button
+
+          onClick={handlePurchaseClick}
+          disabled={isPressed}
+
+          className={clsx("group relative w-full overflow-hidden rounded-full border-8 border-gray-900 bg-linear-to-r/oklch from-sky-300 to-sky-600 px-6 py-6 ease-out focus:ring-24 focus:ring-sky-500/50 focus:outline-none motion-safe:transition-all motion-safe:duration-300 md:border-12 md:px-20 md:py-16", "hover:scale-105 hover:shadow-2xl hover:shadow-sky-500/40", "active:scale-95", isPressed ? "scale-95 cusrsor-not-allowed opacity-80" : "cursor-pointer")}>
           <div className="absolute inset-0 -translate-x-full bg-linear-to-r from-transparent via-white/40 to-transparent ease-out group-hover:translate-x-full motion-safe:transition-transform motion-safe:duration-1000" />
           <div className="relativ z-10 flex items-center justify-center gap-6 md:gap-8 ">
             <span className="font-black-slanted text-4xl tracking-wide text-gray-900 uppercase group-hover:translate-y-1 motion-safe:transition-transform motion-safe:duration-300 md:text-7xl lg:text-9xl">
-              {slice.primary.button_text}
+              { isPressed? (
+                <span className="flex items-center gap-4 md:gap-6">
+                  <LuLoader className="size-12 animate-spin text-gray-900 md:size-16" />
+                  Loading...
+                </span>
+              ) : (
+                slice.primary.button_text
+              )}
             </span>
 
             <div className="hidden group-hover:translate-x-2 group-hover:scale-125 motion-safe:transition-all motion-safe:duration-300 md:block">
