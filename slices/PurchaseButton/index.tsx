4a52f58b9c41a2619ce4510bb9835160fdf8faea
DavideCapuozzo <devsoloweb@gmail.com>
2026-03-23 21:25:37 +0100

    feat: implement product checkout functionality with Stripe integration

--- slices/PurchaseButton/index.tsx
+++ slices/PurchaseButton/index.tsx
@@ -11,6 +11,7 @@ import clsx from "clsx";
 import { LuChevronRight, LuLoader } from "react-icons/lu";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { checkout } from "@/checkout";
 
 gsap.registerPlugin(useGSAP)
 
@@ -26,14 +27,14 @@ const PurchaseButton: FC<PurchaseButtonProps> = ({ slice }) => {
 
   const handlePurchaseClick = async () => {
     setIsPressed(true)
-    // TODO: add checkout logic later
-    await new Promise(resolve => setTimeout(resolve, 2000))
+    await checkout()
     setIsPressed(false)
   };
 
   useGSAP(() => {
-    const handleMouseMove = (event:MouseEvent) => {
-    if(!buttonRef.current || !textRef.current) return;
+    if (!buttonRef.current || !textRef.current) return;
+    const handleMouseMove = (event: MouseEvent) => {
+      if (!buttonRef.current || !textRef.current) return;
       const buttonRect = buttonRef.current.getBoundingClientRect();
       const mouseX = event.clientX - buttonRect.left;
       const buttonWidth = buttonRect.width;
@@ -50,7 +51,7 @@ const PurchaseButton: FC<PurchaseButtonProps> = ({ slice }) => {
     }
 
     const handleMouseLeave = () => {
-      if(!textRef.current) return
+      if (!textRef.current) return
       gsap.to(textRef.current, {
         "--wdth": 85,
         "--wgth": 850,
@@ -64,12 +65,12 @@ const PurchaseButton: FC<PurchaseButtonProps> = ({ slice }) => {
 
 
     gsap.set(textRef.current, {
-      "--wdth" : 85,
-      "--wght" : 850,
+      "--wdth": 85,
+      "--wght": 850,
     })
 
     return () => {
-      if(buttonRef.current){
+      if (buttonRef.current) {
         buttonRef.current?.removeEventListener("mousemove", handleMouseMove)
         buttonRef.current?.removeEventListener("mouseleave", handleMouseLeave)
       }
@@ -99,9 +100,9 @@ const PurchaseButton: FC<PurchaseButtonProps> = ({ slice }) => {
           className={clsx("group relative w-full overflow-hidden rounded-full border-8 border-gray-900 bg-linear-to-r/oklch from-sky-300 to-sky-600 px-6 py-6 ease-out focus:ring-24 focus:ring-sky-500/50 focus:outline-none motion-safe:transition-all motion-safe:duration-300 md:border-12 md:px-20 md:py-16", "hover:scale-105 hover:shadow-2xl hover:shadow-sky-500/40", "active:scale-95", isPressed ? "scale-95 cusrsor-not-allowed opacity-80" : "cursor-pointer")}>
           <div className="absolute inset-0 -translate-x-full bg-linear-to-r from-transparent via-white/40 to-transparent ease-out group-hover:translate-x-full motion-safe:transition-transform motion-safe:duration-1000" />
           <div className="relativ z-10 flex items-center justify-center gap-6 md:gap-8 ">
-            <span ref={textRef} style={{"--wdth": 85, "--wght":850} as React.CSSProperties} className="font-black-slanted text-4xl tracking-wide text-gray-900 uppercase group-hover:translate-y-1 motion-safe:transition-transform motion-safe:duration-300 md:text-7xl lg:text-9xl">
-              { isPressed? (
-                <span 
+            <span ref={textRef} style={{ "--wdth": 85, "--wght": 850 } as React.CSSProperties} className="font-black-slanted text-4xl tracking-wide text-gray-900 uppercase group-hover:translate-y-1 motion-safe:transition-transform motion-safe:duration-300 md:text-7xl lg:text-9xl">
+              {isPressed ? (
+                <span
                   className="flex items-center gap-4 md:gap-6">
                   <LuLoader className="size-12 animate-spin text-gray-900 md:size-16" />
                   Loading...
