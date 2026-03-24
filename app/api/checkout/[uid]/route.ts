355d172d5f417aed5bc46983fcf517fa6cc421c4
DavideCapuozzo <devsoloweb@gmail.com>
2026-03-24 21:46:13 +0100

    fix: update Stripe API version to 2026-02-25.clover in route.ts and page.tsx

--- app/api/checkout/[uid]/route.ts
+++ app/api/checkout/[uid]/route.ts
@@ -5,7 +5,7 @@ import { createClient } from "@/prismicio";
 
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: "2025-07-30.basil"
+    apiVersion: "2026-02-25.clover"
 })
 
 export async function POST(
